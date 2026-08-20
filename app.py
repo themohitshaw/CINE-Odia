9c109d25031290c289116a77bb4e899049d72360
Mohit Kumar Shaw <mohitshaw188@gmail.com>
2026-08-21 00:39:11 +0530

    Commit 02 - Make the Index page search bar and sending data to backend

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' , methods = ["GET" , "POST"])
 def home():
+    if request.method == "POST":
+        movie_list = request.form.getlist("movies")
+        # print(movie_list)
+        new_movie_list = []
+        for movie in movie_list:
+            movie = movie.strip()
+            if movie:
+                new_movie_list.append(movie)
+        # print(new_movie_list)
+    
     return render_template('index.html')
 @app.route('/movies')
 def movies():
